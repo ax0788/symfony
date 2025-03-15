7f314089c230ea46aeebaee329015da4f8c4aec0
ax0788 <ahmy78@gmail.com>
2025-03-15 10:19:07 +0300

    reading record from db, profiler for development debug

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Entity\Product;
 
-/* final */ class ProductController extends AbstractController
+class ProductController extends AbstractController
 {
+/* Used ProductRepository Class to GET data and then display it from the database */
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository->findAll(),
         ]);
     }
+
+/* Reading single Record from db  */
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+
+//we can put an object of the product entity class directly
+ public function show(Product $product): Response
+    {
+       return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+
 }
